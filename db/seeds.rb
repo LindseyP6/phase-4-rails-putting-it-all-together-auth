dc378c1acd3a28e34aa708d18c4975e38d392c73
Lindsey <lindsey.pregent@gmail.com>
2022-03-23 11:56:30 -0400

    commit tree 5dab94bfb2d7ed3b3dafd52cc8a9d23ba889c856

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# User.destroy_all
+# Recipe.destroy_all
+
+# #Seeds for User
+# u1 = User.create(username: "lindsisradd", image_url: "some image", bio: "I am a decent chef, looking for more recipes")
+# u2 = User.create(username: "nicono", image_url: "some image", bio: "I am bored of my current recipes and need more")
+# u3 = User.create(username: "stevo", image_url: "some image", bio: "I can cook but I don't; give me easy recipes")
+
+# #Seeds for Recipe
+# Recipe.create(user_id: u1.id, title: "tacos", instructions: "lots of text goes here for making recipes lots of text goes here for making recipeslots of text goes here for making recipeslots of text goes here for making recipeslots of text goes here for making recipeslots of text goes here for making recipes", minutes_to_complete: 55)
+# Recipe.create(user_id: u2.id, title: "pantry dump casserole", instructions: "lots of text goes here for making recipes lots of text goes here for making recipeslots of text goes here for making recipeslots of text goes here for making recipeslots of text goes here for making recipeslots of text goes here for making recipes", minutes_to_complete: 105)
+# Recipe.create(user_id: u3.id, title: "fish and veggies", instructions: "lots of text goes here for making recipes lots of text goes here for making recipeslots of text goes here for making recipeslots of text goes here for making recipeslots of text goes here for making recipeslots of text goes here for making recipes", minutes_to_complete: 25)
+
